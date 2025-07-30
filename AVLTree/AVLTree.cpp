4f3df65e220e91ef44f628266d0c8c43716ff9e4
Raed Salah Bzour <raedbzour77@gmail.com>
2025-07-30 16:08:09 +0300

    managing the rotation for balancing

--- AVLTree/AVLTree.cpp
+++ AVLTree/AVLTree.cpp
@@ -28,6 +28,27 @@ public:
     int getBalanceFactor(Node* node) {
         return node == nullptr ? 0 : getHeight(node->left) - getHeight(node->right);
     }
+    Node* RightRotate(Node* unbalancedNode)
+    {
+        Node* newRoot = unbalancedNode->left;
+        Node* movedSubtree = newRoot->right;
+
+        newRoot->right = unbalancedNode;
+        unbalancedNode->left = movedSubtree;
+        updateHeight(unbalancedNode);
+        updateHeight(newRoot);
+        return newRoot;
+    }
+    Node* LeftRotate(Node* unbalancedNode)
+    {
+        Node* newRoot = unbalancedNode->right;
+        Node* movedSubtree = newRoot->left;
+        newRoot->left= unbalancedNode;
+        unbalancedNode->right = movedSubtree;
+        updateHeight(unbalancedNode);
+        updateHeight(newRoot);
+        return newRoot;
+    }
     
 };
 int main()
